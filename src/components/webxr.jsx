39168b9321172e8f260c3e68569ff54e62e7c0f7
saiakhilvarm9492384807 <varmaakhil27@gmail.com>
2024-03-28 22:59:12 +0530

    major chngs in ui.

---
+++ src/components/webxr.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import 'aframe';
+
+import { Entity, Scene } from 'aframe-react';
+import { Row, Col } from 'react-bootstrap';
+
+export const WebXR = () => {
+    return (
+        <Row>
+            <Col xs={6}>
+                <model-viewer
+                    autoplay 
+                    ar
+                    ar-modes="webxr scene-viewer quick-look"
+                    src={'/models/vr_headset.glb'}
+                    loading="eager"
+                    shadow-intensity="1"
+                    auto-rotate 
+                    camera-controls
+                    style={{
+                        width: '45vw', height: '45vh', margin: '3%',
+                        background: 'transparent', borderRadius: '5px',
+                    }}
+                ></model-viewer>
+            </Col>
+
+            <Col xs={6}>
+                <Scene>
+                    <Entity geometry={{primitive: 'torus'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}} />
+                    <Entity light={{type: 'point'}} />
+                    <Entity gltf-model={{src: '/models/laptop.gltf'}} />
+                    <Entity text={{value: 'Hello, WebVR!'}} />
+                </Scene>
+            </Col>
+        </Row>
+    );
+};
+
+export default WebXR;
