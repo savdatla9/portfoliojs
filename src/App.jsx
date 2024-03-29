5ad803249092286a3af7c294e184dde1453244aa
Sai Akhil Varma Datla <72064595+sai2702@users.noreply.github.com>
2024-03-29 10:02:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Home from './components/home.jsx';
 import WebXR from './components/webxr.jsx';
@@ -12,10 +12,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 const App = () => {
+    const [type, setType] = useState('ar');
+    
     return (
       <div>
-        <Home />
-        <WebXR />
+        <Home xr={type} setxr={setType} />
+        <WebXR xr={type} />
       </div>
     )
 }
